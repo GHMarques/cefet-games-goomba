cea4572b4bb6b6686331f0ca765664e9450f7de6
GHMarques <gustavo.marques2011@gmail.com>
2017-08-17 20:41:05 -0300

    Desafio 1: animação em 4 direções

--- core/src/br/cefetmg/games/Goomba.java
+++ core/src/br/cefetmg/games/Goomba.java
@@ -17,7 +17,7 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 /**
  *
- * @author gusta
+ * @author gustavo
  */
 public class Goomba extends ApplicationAdapter{
     //private Sprite sprGoomba;
@@ -26,9 +26,9 @@ public class Goomba extends ApplicationAdapter{
     private float fltMaxHeight, fltHeight = 24;
     
     private TextureRegion[][] quadrosDaAnimacao;
-    private Animation andarParaFrente;
+    private Animation[] andar = new Animation[5];
     private float tempoDaAnimacao;
-    
+    private int intMovimentoAtual = 2;
     /**
      * Contrutor da classe Goomba
      * Conforme o enunciado da pratica, seta a posicao inicial em (30,10)
@@ -38,14 +38,43 @@ public class Goomba extends ApplicationAdapter{
         quadrosDaAnimacao = TextureRegion.split(texGoomba, (int)fltWidth, (int)fltHeight);
         fltPosition[0] = 30; //Coordenada x inicial
         fltPosition[1] = 10; //Coordenada y inicial
-        
-        andarParaFrente = new Animation(0.1f,
+        //Andar para baixo
+        andar[0] = new Animation(0.1f,
                 quadrosDaAnimacao[0][0],
                 quadrosDaAnimacao[0][1],
                 quadrosDaAnimacao[0][2],
                 quadrosDaAnimacao[0][3],
                 quadrosDaAnimacao[0][4]);
-        andarParaFrente.setPlayMode(PlayMode.LOOP_PINGPONG);
+        andar[0].setPlayMode(PlayMode.LOOP_PINGPONG);
+        //Andar para direita
+        andar[1] = new Animation(0.1f,
+                quadrosDaAnimacao[1][0],
+                quadrosDaAnimacao[1][1],
+                quadrosDaAnimacao[1][2],
+                quadrosDaAnimacao[1][3],
+                quadrosDaAnimacao[1][4]);
+        andar[1].setPlayMode(PlayMode.LOOP_PINGPONG);
+        //Andar para cima
+        andar[2] = new Animation(0.1f,
+                quadrosDaAnimacao[2][0],
+                quadrosDaAnimacao[2][1],
+                quadrosDaAnimacao[2][2],
+                quadrosDaAnimacao[2][3],
+                quadrosDaAnimacao[2][4]);
+        andar[2].setPlayMode(PlayMode.LOOP_PINGPONG);
+        //Andar para esquerda
+        andar[3] = new Animation(0.1f,
+                quadrosDaAnimacao[3][0],
+                quadrosDaAnimacao[3][1],
+                quadrosDaAnimacao[3][2],
+                quadrosDaAnimacao[3][3],
+                quadrosDaAnimacao[3][4]);
+        andar[3].setPlayMode(PlayMode.LOOP_PINGPONG);
+        //Andar para esquerda
+        andar[4] = new Animation(0.1f,
+                quadrosDaAnimacao[0][2]);
+        andar[4].setPlayMode(PlayMode.LOOP_PINGPONG);
+        
         tempoDaAnimacao = 0;
     }
     
@@ -78,7 +107,7 @@ public class Goomba extends ApplicationAdapter{
      * Metodo responsavel por desenhar o Goomba
      */
     public void render(SpriteBatch batch) {
-        batch.draw((TextureRegion) andarParaFrente.getKeyFrame(tempoDaAnimacao), fltPosition[0], fltPosition[1]);
+        batch.draw((TextureRegion) andar[intMovimentoAtual].getKeyFrame(tempoDaAnimacao), fltPosition[0], fltPosition[1]);
     }
     
     /**
@@ -88,25 +117,32 @@ public class Goomba extends ApplicationAdapter{
     public void update(float delta) {
         //tempoDaAnimacao
         tempoDaAnimacao += Gdx.graphics.getDeltaTime();
+        //Para a animacao quando nenhuma tecla e' pressionada
+        intMovimentoAtual = 4;
         //Movimento vertical para cima do Goomba
         if(Gdx.input.isKeyPressed(Input.Keys.UP) || Gdx.input.isKeyPressed(Input.Keys.W)){
+            intMovimentoAtual = 2;
             if(fltPosition[1] < (fltMaxHeight - fltHeight))
                 fltPosition[1]++;
         }
         //Movimento vertical para baixo do Goomba
         if(Gdx.input.isKeyPressed(Input.Keys.DOWN) || Gdx.input.isKeyPressed(Input.Keys.S)){
+            intMovimentoAtual = 0;
             if(fltPosition[1] > 0)
                 fltPosition[1]--;
         }
         //Movimento horizontal para direita do Goomba
         if(Gdx.input.isKeyPressed(Input.Keys.RIGHT) || Gdx.input.isKeyPressed(Input.Keys.D)){
+            intMovimentoAtual = 1;
             if(fltPosition[0] < (fltMaxWidth - fltWidth))
                 fltPosition[0]++;
         }
         //Movimento vertical para baixo do Goomba
         if(Gdx.input.isKeyPressed(Input.Keys.LEFT) || Gdx.input.isKeyPressed(Input.Keys.A)){
+            intMovimentoAtual = 3;
             if(fltPosition[0] > 0)
                 fltPosition[0]--;
         }
+            
     }
 }
